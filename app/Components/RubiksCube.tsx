4c053bcbedc63a8312f7ab059c96f9bdc95046c7
Amir Kakavand <amirkkvnd99@gmail.com>
2024-12-14 18:53:58 +0330

    Modified the logic behind separating the cubelets

--- app/Components/RubiksCube.tsx
+++ app/Components/RubiksCube.tsx
@@ -3,7 +3,7 @@ import { OrbitControls } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import { Suspense, useState, useEffect } from "react";
-import { Object3D, Object3DEventMap } from 'three'
+import { Object3D, Object3DEventMap } from "three";
 import Cubelet from "./Cubelet";
 
 const RubiksCube = () => {
@@ -11,42 +11,44 @@ const RubiksCube = () => {
   // Log the model if it exists
   useEffect(() => {
     if (model) {
-        console.log(model);
+      // console.log(model);
     }
-}, [model]);
+  }, [model]);
 
-const [faces, setFaces] = useState([])
+  // let cubelets: Object3D[] = [];
+  const [cubelets, setCubelets] = useState<Object3D[]>([])
 
-useEffect(() => {
-  if (model && model.scene) {
-    // Cubelets
-    const cubelets: Object3D[] = model.scene.children as Object3D[];
+  const [faces, setFaces] = useState<Object3D[]>([]);
 
-    // IDs for filtering
-    const H1_IDs = [15, 16, 21, 10, 22, 23, 2, 3];
+  useEffect(() => {
+    if (model && model.scene) {
+      // Cubelets
+      // cubelets = model.scene.children as Object3D[];
+      setCubelets(model.scene.children as Object3D[])
+      // console.log(cubelets);
 
-    // Filter cubelets based on IDs
-    const H1 = cubelets.filter(cubelet => H1_IDs.includes(cubelet.id));
+      // IDs for filtering
+      const H1_IDs = [15, 16, 21, 10, 22, 23, 2, 3];
 
-    setFaces(faces.H1)
+      // Filter cubelets based on IDs
+      const H1 = cubelets && cubelets.filter((cubelet) => H1_IDs.includes(cubelet.id));
 
-}
-}, [model])
-  
-  // Cubelets
-  // const cubelets:Object3D<Object3DEventMap>[] = model.scene.children;
-  
-  // const H1_IDs = [15, 16, 21, 10, 22, 23, 2, 3, 5]
-
-  // const [faces, setFaces] = useState[
-  //   h1: {children: cubelets.filter(cubelet => H1_IDs.includes(cubelet.id))}
-  // ]
+      // setFaces(faces.push(H1))
+      setFaces(prevFaces => {
+        const updatedFaces = [...prevFaces, ...H1]; // Combine previous faces with new ones
+        console.log("Updated faces:", H1); // Log the updated faces state
+        return updatedFaces; // Return the new state
+      });
+    }
+  }, [model]);
 
   return (
-      <Suspense fallback={null}>
-        {cubelets.map((cubelet) => <Cubelet key={cubelet.id} cubeletMeshData={cubelet} />)}
-        <OrbitControls />
-      </Suspense>
+    <Suspense fallback={null}>
+      {cubelets && cubelets.map((cubelet) => (
+        <Cubelet key={cubelet.id} cubeletMeshData={cubelet} />
+      ))}
+      <OrbitControls />
+    </Suspense>
   );
 };
 
